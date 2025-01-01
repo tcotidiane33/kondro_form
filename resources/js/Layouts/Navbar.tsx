4bb4cf4df1861ad488654d5bd4e9ced3f08dd5d2
tcotidiane33 <tcotidiane33@gmail.com>
2025-01-01 02:56:40 +0100

    Ajout d'un menu utilisateur dans la barre de navigation avec des options de tableau de bord, paramètres et déconnexion; ajout d'un champ image à l'interface utilisateur

--- resources/js/Layouts/Navbar.tsx
+++ resources/js/Layouts/Navbar.tsx
@@ -10,6 +10,7 @@ const Navbar = () => {
     const auth = usePage().props.auth;
 
     const user = auth.user;
+    const isAuthenticated = !!user;
 
     // const getDashboardRoute = () => {
     //     if (!user) {
@@ -122,6 +123,95 @@ const Navbar = () => {
                         >
                             Individual
                         </Link>
+                        {isAuthenticated && (
+                            <div className="grig items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+                                <button
+                                    type="button"
+                                    className="grid text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
+                                    id="user-menu-button"
+                                    aria-expanded="false"
+                                    onClick={() => setDropdownOpen(!dropdownOpen)}
+                                >
+                                    <span className="sr-only">Open user menu</span>
+                                    <img
+                                        className="w-8 h-8 rounded-full"
+                                        src={user.image}
+                                        alt="user photo"
+                                    />
+                                </button>
+                                {/* Dropdown menu */}
+                                {dropdownOpen && (
+                                    // absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg py-2 z-20 transition duration-300 ease-in-out transform origin-top-right ${dropdownOpen ? 'scale-100' : 'scale-95'}
+                                        <div
+                                            className={`absolute right-0 mt-2 z-20  text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600 transition duration-300 ease-in-out transform origin-top-right ${dropdownOpen ? 'scale-100' : 'scale-95'}`}
+                                            id="user-dropdown"
+                                        >
+                                            <div className="px-4 py-3">
+                                                <span className="block text-sm text-gray-900 dark:text-white">{user.name}</span>
+                                                <span className="block text-sm text-gray-500 truncate dark:text-gray-400">{user.email}</span>
+                                            </div>
+                                            <ul className="py-2" aria-labelledby="user-menu-button">
+                                                <li>
+                                                    <Link
+                                                        href={route('dashboard')}
+                                                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                                                    >
+                                                        Dashboard
+                                                    </Link>
+                                                </li>
+                                                <li>
+                                                    <Link
+                                                        href={route('dashboard')}
+                                                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                                                    >
+                                                        Settings
+                                                    </Link>
+                                                </li>
+                                                <li>
+                                                    <Link
+                                                        href={route('dashboard')}
+                                                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                                                    >
+                                                        Earnings
+                                                    </Link>
+                                                </li>
+                                                <li>
+                                                    <Link
+                                                        href={route('logout')}
+                                                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                                                    >
+                                                        Sign out
+                                                    </Link>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                )}
+                                <button
+                                    data-collapse-toggle="navbar-user"
+                                    type="button"
+                                    className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                                    aria-controls="navbar-user"
+                                    aria-expanded="false"
+                                >
+                                    <span className="sr-only">Open main menu</span>
+                                    <svg
+                                        className="w-5 h-5"
+                                        aria-hidden="true"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 17 14"
+                                    >
+                                        <path
+                                            stroke="currentColor"
+                                            stroke-linecap="round"
+                                            stroke-linejoin="round"
+                                            stroke-width="2"
+                                            d="M1 1h15M1 7h15M1 13h15"
+                                        />
+                                    </svg>
+                                </button>
+                            </div>
+                        )}
                     </nav>
                 </div>
 
--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -4,6 +4,7 @@ export interface User {
     name: string;
     email: string;
     email_verified_at?: string;
+    image: string;
 }
 
 export type PageProps<
